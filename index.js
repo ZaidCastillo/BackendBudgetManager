fcac33b2d2d941f520eb31a57bd870f2c450eb12
Zaid Castillo <czaid798@gmail.com>
2026-02-28 17:55:28 -0600

    Allow

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 const allowed = [
+  'https://backendbudgetmanager.onrender.com',
   'http://localhost:3000',
   'http://localhost:3001',
   'http://localhost:5173',
